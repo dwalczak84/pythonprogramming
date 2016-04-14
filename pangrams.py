cadcdeaec0eca66d40f710a01d6cbf87bd023535
Dariusz Walczak <dariusz.w84@gmail.com>
2016-04-14 19:42:52 +0100

    Update pangrams.py

--- pangrams.py
+++ pangrams.py
@@ -10,12 +10,12 @@
 # Input Format
 # Input consists of a string ss.
 # Constraints 
-3 Length of ss can be at most 103103 (1≤|s|≤103)(1≤|s|≤103) and it may contain 
+# Length of ss can be at most 103103 (1≤|s|≤103)(1≤|s|≤103) and it may contain 
 # spaces, lower case and upper case letters. Lower-case and upper-case 
 # instances of a letter are considered the same.
 
 # Output Format
-3 Output a line containing pangram if ss is a pangram, otherwise output not 
+# Output a line containing pangram if ss is a pangram, otherwise output not 
 # pangram.
 # Sample Input
 # Input #1
